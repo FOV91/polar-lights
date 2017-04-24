bd879a3cc10b2c75a01bdb1b82194e560597ac06
Oleg Fedorov <fovspb@mail.ru>
2017-04-24 15:20:23 +0300

    Owl-carousel подключение

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
   // весь ваш код
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
-
+  $('.owl-carousel').owlCarousel();
 });
