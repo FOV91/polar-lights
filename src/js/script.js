f2c8e48fb5dd9c261e85865b5a1503fa690922e1
Oleg Fedorov <fovspb@mail.ru>
2017-04-26 02:21:27 +0300

    Блок цены готов

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,46 @@
 $(document).ready(function(){
   // весь ваш код
 
-  $('#carouselOne').owlCarousel({
-  	item:1,
-  	nav: true,
-  	dots: true,
-  });
+  // $('#carouselOne').owlCarousel({
+  // 	item:1,
+  // 	nav: true,
+  // 	dots: true,
+  // });
+
+
+    // $('#tab').on('click', function(event){
+    // 	event.preventDefault()
+	   //  $(this)
+	   //    .parent()
+	   //    .addClass('regions--active')
+	   //    .siblings()
+	   //    .removeClass('regions--active')
+	      
+	    // var activeId = $(this).attr('rel') // tabs2
+	    // var activePanel = $('#' + activeId) //tab2
+	    // var otherPanel = activePanel.siblings();
+	    
+	    // otherPanel.hide();
+	    
+	    // activePanel.addClass('d_active')
+	    //   .fadeIn();
+	// });
+
+
+	$('ul.residence-tabs__list').each(function() {
+	    $(this).find('li').each(function(i) {
+	      $(this).click(function(){
+	        $(this)
+	        	.addClass('active-li')
+	        	.siblings()
+	        	.removeClass('active-li')
+	          	.closest('div.residence-tabs')
+	          	.find('div.residence-tabs__content')
+	          	.removeClass('active-tab')
+	          	.eq(i)
+	          	.addClass('active-tab');
+	      	});
+	    });
+  	});
+
 });
